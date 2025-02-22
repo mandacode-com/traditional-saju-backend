c5ff64543d9fc6b999f738cc0692534b8aee9321
sauce <dadad1004@naver.com>
2025-02-23 08:29:15 +0900

    chore: import jwt module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { validate } from './config/validate';
 import { YearlySajuModule } from './modules/yearly_saju.module';
 import { AppController } from './app.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { Config } from './schemas/config.schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validate: validate,
       isGlobal: true,
+    }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<Config, true>) => ({
+        secret: configService.get<Config['jwt']>('jwt').secret,
+      }),
+      global: true,
     }),
     YearlySajuModule,
   ],
