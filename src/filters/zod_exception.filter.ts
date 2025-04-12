1e62b0f3e15ee3b0bcfa056c3c995d0227314bf4
sauce-Git <dadad1004@naver.com>
2025-04-13 02:49:17 +0900

    feat: add exception filters

---
+++ src/filters/zod_exception.filter.ts
@@ -0,0 +1,32 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { ResponseError } from 'src/interfaces/response.interface';
+import { ZodError } from 'zod';
+
+@Catch(ZodError)
+export class ZodExceptionFilter implements ExceptionFilter {
+  catch(exception: ZodError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = HttpStatus.BAD_REQUEST;
+    const message = exception.errors.map((error) => error.message).join(', ');
+    const errors = 'Validation Error';
+
+    const errorResponse: ResponseError = {
+      message: message,
+      data: {
+        error: errors,
+        path: request.url,
+        timestamp: new Date().toISOString(),
+      },
+    };
+
+    response.status(status).json(errorResponse);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Config } from './schemas/config.schema';
+import { HttpExceptionFilter } from './filters/http_exception.filter';
+import { PrismaExceptionFilter } from './filters/prisma_exception.filter';
+import { ZodExceptionFilter } from './filters/zod_exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,5 +13,12 @@ async function bootstrap() {
   const config = app.get(ConfigService<Config, true>);
 
   await app.listen(config.get<Config['server']>('server').port);
+
+  // Implement global exception filter
+  app.useGlobalFilters(
+    new HttpExceptionFilter(),
+    new PrismaExceptionFilter(),
+    new ZodExceptionFilter(),
+  );
 }
 bootstrap().catch(console.error);
