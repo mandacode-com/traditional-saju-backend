21bb46a7fdce00e7c758db9c8b3365ab4114483f
sauce-Git <dadad1004@naver.com>
2025-04-13 02:49:27 +0900

    feat: add zod validation pipe

---
+++ src/pipes/zod_validation.pipe.ts
@@ -0,0 +1,23 @@
+import {
+  BadRequestException,
+  Injectable,
+  Logger,
+  PipeTransform,
+} from '@nestjs/common';
+import { z, ZodSchema } from 'zod';
+
+@Injectable()
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private validator: ZodSchema) {}
+  async transform(value: any): Promise<z.infer<typeof this.validator>> {
+    return await this.validator.parseAsync(value).catch((error) => {
+      Logger.error(
+        `ZodValidationPipe: Validation failed for value ${JSON.stringify(
+          value,
+        )}: ${error}`,
+        'ZodValidationPipe',
+      );
+      throw new BadRequestException('Validation failed');
+    });
+  }
+}
