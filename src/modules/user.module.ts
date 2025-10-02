c1161b769c1d08c103d82da81a8f41925c46e626
sauce-git <dadad02100@gmail.com>
2025-10-02 12:40:35 +0900

    feat: update user module

--- src/modules/user.module.ts
+++ src/modules/user.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from '../services/user.service';
 import { PrismaModule } from './prisma.module';
+import { UserController } from '../controllers/user.controller';
+import { IdpModule } from './idp.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, IdpModule],
+  controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
 })
